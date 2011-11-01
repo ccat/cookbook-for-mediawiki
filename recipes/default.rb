70f0b56da54289bc8a35bd920b3a9dd5423d29f4
ccat <deadlack@gmail.com>
2011-11-01 23:53:41 +0900

    Previous version crash LocalSettings.php, so modified.

--- recipes/default.rb
+++ recipes/default.rb
@@ -33,11 +33,13 @@ script "set_mediawiki" do
   php maintenance/install.php --dbname #{node[:mediawiki][:dbname]} --dbpass #{node[:mediawiki][:dbpass]}  --dbserver #{node[:mediawiki][:dbserver]} --dbuser #{node[:mediawiki][:dbuser]} --installdbpass #{node[:mediawiki][:installdbPass]}  --pass #{node[:mediawiki][:dbAdminPass]} --installdbuser root --lang #{node[:mediawiki][:lang]}  #{node[:mediawiki][:sitename]} #{node[:mediawiki][:dbAdminUser]}
   mysql -u root -p#{node[:mediawiki][:installdbPass]} < /tmp/set_pass.sql
   mv LocalSettings.php LocalSettings_autogenerate.php
-  sed -e "s/maintenance\\/install.php/#{node[:mediawiki][:url]}/g" LocalSettings_autogenerate.php > LocalSettings.php
-  sed -e "/\$wgEmergencyContact/D" LocalSettings.php > LocalSettings.php
-  sed -e "/\$wgPasswordSender/D" LocalSettings.php > LocalSettings.php
-  echo "\$wgEmergencyContact = #{node[:mediawiki][:email]}" >> LocalSettings.php
-  echo "\$wgPasswordSender = #{node[:mediawiki][:email]}" >> LocalSettings.php
+  sed -e "s/maintenance\\/install.php/#{node[:mediawiki][:url]}/g" LocalSettings_autogenerate.php > LocalSettings_temp.php
+  sed -e "/\\$wgEmergencyContact/D" LocalSettings_temp.php > LocalSettings_temp2.php
+  sed -e "/\\$wgPasswordSender/D" LocalSettings_temp2.php > LocalSettings.php
+  echo "\\$wgEmergencyContact = #{node[:mediawiki][:email]}" >> LocalSettings.php
+  echo "\\$wgPasswordSender = #{node[:mediawiki][:email]}" >> LocalSettings.php
+  rm -rf LocalSettings_temp.php
+  rm -rf LocalSettings_temp2.php
   EOH
 end
 
